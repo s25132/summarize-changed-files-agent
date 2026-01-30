3e9207ee2ce22057fee8d64c297faca8dbdea3ce
jhass <s25132@pjwstk.edu.pl>
2026-01-30 10:23:48 +0100

    List files from github workspace

--- app.py
+++ app.py
@@ -1,4 +1,16 @@
 import sys
+import os
+
 
 name = sys.argv[1] if len(sys.argv) > 1 else "World"
 print(f"Hello, {name}! 👋 From Docker Action")
+
+
+WORKSPACE = "/github/workspace"
+
+print("📂 Files in repository:\n")
+
+for root, dirs, files in os.walk(WORKSPACE):
+    for name in files:
+        path = os.path.join(root, name)
+        print(path.replace(WORKSPACE + "/", ""))
